8fcecde015539ea6e6ca7e1acb4c001aa1e00172
Puskunalis <47086537+Puskunalis@users.noreply.github.com>
2020-11-09 15:47:03 +0200

    Update 3rd task and add input/output files

--- 3-3.c
+++ 3-3.c
@@ -1,98 +1,89 @@
 // Vilius Puskunalis 5 grupe 3 uzduotis
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+#define ARGS_LENGTH 2
 
 #define ASCII_SPACE 32
 #define MAX_LINE_SIZE 256
 
-/*
-**  Return codes:
-**  0 - success
-**  1 - read error
-**  2 - write error
-*/
-int connectWords(char* inputFileName, char* outputFileName)
+#define SUCCESS 0
+#define READ_ERROR 1
+#define WRITE_ERROR 2
+#define MEMORY_ERROR 3
+
+char* connectLine(char* line)
 {
-    FILE* inputFile;
-
-    if (!strcmp(inputFileName, "stdin"))
+    for (int i = 0; line[i + 3] != 0; ++i)
     {
-        inputFile = stdin;
-    }
-    else
-    {
-        inputFile = fopen(inputFileName, "r");
-
-        if (inputFile == NULL)
+        // Checking whether symbols separated by a dash are not spaces
+        if (line[i] != ASCII_SPACE && line[i + 1] == '-' && line[i + 2] != ASCII_SPACE)
         {
-            return 1;
-        }
-    }
-
-    FILE* outputFile;
-
-    if (!strcmp(outputFileName, "stdout"))
-    {
-        outputFile = stdout;
-    }
-    else
-    {
-        outputFile = fopen(outputFileName, "w");
-
-        if (outputFile == NULL)
-        {
-            fclose(inputFile);
-            return 2;
-        }
-    }
-
-    char* line = malloc(MAX_LINE_SIZE + 1);
-
-    while (!feof(inputFile))
-    {
-        line = fgets(line, MAX_LINE_SIZE + 1, inputFile);
-
-        if (line != NULL)
-        {
-            for (int i = 0; line[i + 3] != 0; ++i)
+            // Move the rest of the string, this way deleting the dash
+            for (int j = i + 1; line[j] != 0; ++j)
             {
-                // Checking whether symbols separated by a dash are not spaces
-                if (line[i] != ASCII_SPACE && line[i + 1] == '-' && line[i + 2] != ASCII_SPACE)
-                {
-                    // Move the rest of the string, this way deleting the dash
-                    for (int j = i + 1; line[j] != 0; ++j)
-                    {
-                        line[j] = line[j + 1];
-                    }
-
-                    --i;
-                }
+                line[j] = line[j + 1];
             }
 
-            fputs(line, outputFile);
+            --i;
         }
+    }
+
+    return line;
+}
+
+int fConnectLines(char* inputFileName, char* outputFileName)
+{
+    FILE *inputFile = fopen(inputFileName, "r");
+    if (inputFile == NULL)
+    {
+        return READ_ERROR;
+    }
+
+    FILE *outputFile = fopen(outputFileName, "w");
+    if (outputFile == NULL)
+    {
+        fclose(inputFile);
+        return WRITE_ERROR;
+    }
+
+    char *line = (char*) malloc(MAX_LINE_SIZE + 1);
+    if (line == NULL)
+    {
+        fclose(inputFile);
+        fclose(outputFile);
+        return MEMORY_ERROR;
+    }
+
+    while (fgets(line, MAX_LINE_SIZE + 1, inputFile))
+    {
+        fputs(connectLine(line), outputFile);
     }
 
     free(line);
     fclose(inputFile);
     fclose(outputFile);
-    return 0;
+    return SUCCESS;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc > 2)
+    if (argc > ARGS_LENGTH)
     {
-        int result = connectWords(argv[1], argv[2]);
-
-        if (result == 1)
+        switch (fConnectLines(argv[1], argv[2]))
         {
-            printf("Input file not found!\n");
-        }
-        else if (result == 2)
-        {
-            printf("Cannot create output file!\n");
+            case READ_ERROR:
+                printf("Input file not found!\n");
+                break;
+            case WRITE_ERROR:
+                printf("Cannot create output file!\n");
+                break;
+            case MEMORY_ERROR:
+                printf("Out of memory!\n");
+                break;
+            case SUCCESS:
+                printf("All done.\n");
+                break;
         }
     }
     else
